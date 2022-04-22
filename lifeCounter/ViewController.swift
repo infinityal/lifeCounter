c1e6881918b4d6c970f4bee8a093b38a27cfc48d
infinityal <zhjzh15@uw.edu>
2022-04-21 18:44:05 -0700

    Initial Commit

---
+++ lifeCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifeCounter
+//
+//  Created by 郑智健 on 4/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
