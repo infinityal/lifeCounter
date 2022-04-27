f705ec42e206f476ea46cafacb20674e5add0ee6
infinityal <zhjzh15@uw.edu>
2022-04-26 23:39:20 -0700

    ddl extension

--- lifeCounter/ViewController.swift
+++ lifeCounter/ViewController.swift
@@ -8,80 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var p1: UILabel!
-    @IBOutlet weak var p1PlusOne: UIButton!
-    @IBOutlet weak var p1PlusFive: UIButton!
-    @IBOutlet weak var p1MinusOne: UIButton!
-    @IBOutlet weak var p1MinusFive: UIButton!
-    @IBOutlet weak var p2: UILabel!
-    @IBOutlet weak var p2PlusOne: UIButton!
-    @IBOutlet weak var p2MinusOne: UIButton!
-    @IBOutlet weak var p2PlusFive: UIButton!
-    @IBOutlet weak var p2MinusFive: UIButton!
-    
+    @IBOutlet weak var p1: UIView!
+    @IBOutlet weak var p2: UIView!
+    @IBOutlet weak var p3: UIView!
+    @IBOutlet weak var p4: UIView!
+    @IBOutlet weak var p5: UIView!
+    @IBOutlet weak var p6: UIView!
+    @IBOutlet weak var p7: UIView!
+    @IBOutlet weak var p8: UIView!
+    @IBOutlet weak var p1L: UILabel!
+    @IBOutlet weak var p2L: UILabel!
+    @IBOutlet weak var p3L: UILabel!
+    @IBOutlet weak var p4L: UILabel!
+    @IBOutlet weak var p5L: UILabel!
+    @IBOutlet weak var p6L: UILabel!
+    @IBOutlet weak var p7L: UILabel!
+    @IBOutlet weak var p8L: UILabel!
+    @IBOutlet weak var addBttn: UIImageView!
+    @IBOutlet weak var historyBttn: UIButton!
     @IBOutlet weak var lostLabel: UILabel!
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        p5.isHidden = true
+        p6.isHidden = true
+        p7.isHidden = true
+        p8.isHidden = true
     }
     
-    var p1Life = 20;
-    var p2Life = 20;
-    
-    func updateValue(player: Int) {
+    func updateValue(player: Int, value: Int) {
         if player == 1 {
-            p1.text = String(p1Life)
-            if p1Life <= 0 {
+            p1L.text = String(value)
+            if value <= 0 {
                 lostLabel.text = "Player 1 LOSES!"
             }
         } else {
-            p2.text = String(p2Life)
-            if p2Life <= 0 {
+            
+            if value <= 0 {
                 lostLabel.text = "Player 2 LOSES!"
             }
         }
     }
-    
-    @IBAction func p1MinusFive(_ sender: UIButton) {
-        p1Life -= 5;
-        updateValue(player: 1);
-    }
-    
-    @IBAction func p1MinusOne (_ sender: UIButton) {
-        p1Life -= 1;
-        updateValue(player: 1);
-    }
-    
-    @IBAction func p2MinusOne(_ sender: UIButton) {
-        p2Life -= 1;
-        updateValue(player: 2);
-    }
-    
-    @IBAction func p2MinusFive(_ sender: UIButton) {
-        p2Life -= 5;
-        updateValue(player: 2);
-    }
-    
-    @IBAction func p1PlusOne(_ sender: UIButton) {
-        p1Life += 1;
-        updateValue(player: 1);
-    }
-    
-    @IBAction func p1PlusFive(_ sender: UIButton) {
-        p1Life += 5;
-        updateValue(player: 1);
-    }
-    
-    @IBAction func p2PlusOne(_ sender: UIButton) {
-        p2Life += 1;
-        updateValue(player: 2);
-    }
-    
-    @IBAction func p2PlusFive(_ sender: UIButton) {
-        p2Life += 5;
-        updateValue(player: 2);
-    }
+
 
 
 }
